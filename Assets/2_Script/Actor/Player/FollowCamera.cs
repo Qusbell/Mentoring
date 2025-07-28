e936e62f22e17600ff8db27d4511e8d3ae579ab0
Qusbell <01024072245a@gmail.com>
2025-07-28 14:55:30 +0900

    카메라

--- Assets/2_Script/Actor/Player/FollowCamera.cs
+++ Assets/2_Script/Actor/Player/FollowCamera.cs
@@ -6,14 +6,15 @@ public class FollowCamera : MonoBehaviour
 {
     [SerializeField] protected Transform target; // 따라갈 타겟(플레이어)
     [SerializeField] protected Vector3 offset;
-    [SerializeField] protected Vector3 rotation;
-    
+
     void LateUpdate()
     {
-        if(target != null)
+        if (target != null)
         {
+            // 위치 이동
             transform.position = target.position + offset;
-            transform.rotation = Quaternion.Euler(rotation);
+            // 타겟을 항상 바라보게
+            transform.LookAt(target.position);
         }
     }
 }
